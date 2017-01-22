f5c6e1f9a81bada8563bf92ec4a9ca1e7397a947
ushawn <ushawn@jbnu.ac.kr>
2017-01-22 16:09:10 +0900

    설정 페이지 수정

--- cozyhouzz-app/src/pages/mypage/mypage.ts
+++ cozyhouzz-app/src/pages/mypage/mypage.ts
@@ -19,7 +19,7 @@ export class MyPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
 
-  pages: Array<{title: string, component: any, flag: boolean, ios: string, md: string}>;
+  pages: Array<{title: string, component: any, flag: boolean, ios: string, md: string, detail: string}>;
 
   constructor(public userService: UserService, private events: Events,
               public navCtrl: NavController, public params: NavParams, public menu:MenuController,
@@ -27,13 +27,19 @@ export class MyPage {
 
 
     this.pages = [
-      {title: '방 검색 설정', component: RoomSettingPage, flag: true, ios: 'ios-list', md: 'md-list'},
-      {title: '방 검색 설정', component: RoomSettingPage, flag: false, ios: 'ios-list', md: 'md-list'},
-      {title: '로그인', component: LoginPage, flag: true, ios: 'ios-list', md: 'md-list'},
-      {title: '회원가입', component: RegistrationPage, flag: true, ios: 'ios-list', md: 'md-list'},
-      {title: '1:1 문의 하기', component: QuestionListPage, flag: false, ios: 'ios-list', md: 'md-list'},
-      {title: '컨설팅 내역', component: ConsultingListPage, flag: false, ios: 'ios-list', md: 'md-list'},
-      {title: '계정 정보 보기', component: UserInfoDetailPage, flag: false, ios: 'ios-list', md: 'md-list'}
+      {title: '방 검색 설정', component: RoomSettingPage, flag: true, ios: 'ios-settings', md: 'md-settings', detail: '방 검색 조건을 설정합니다. 보증금, 월세, 방 타입'},
+      {title: '방 검색 설정', component: RoomSettingPage, flag: false, ios: 'ios-settings', md: 'md-settings', detail: '방 검색 조건을 설정합니다. 보증금, 월세, 방 타입'},
+
+      {title: '찜 목록', component: RoomSettingPage, flag: true, ios: 'ios-heart', md: 'md-heart', detail: '관심있는 방 또는 인테리어 목록입니다.'},
+      {title: '찜 목록', component: RoomSettingPage, flag: false, ios: 'ios-heart', md: 'md-heart', detail: '관심있는 방 또는 인테리어 목록입니다.'},
+
+      {title: '최근 본 방', component: RoomSettingPage, flag: true, ios: 'ios-time', md: 'md-time', detail: '최근에 본 방 또는 인테리어 목록입니다.'},
+      {title: '최근 본 방', component: RoomSettingPage, flag: false, ios: 'ios-time', md: 'md-time', detail: '최근에 본 방 또는 인테리어 목록입니다.'},
+      {title: '로그인', component: LoginPage, flag: true, ios: 'ios-log-in', md: 'md-log-in', detail: '로그인 페이지입니다.'},
+      {title: '회원가입', component: RegistrationPage, flag: true, ios: 'ios-list', md: 'md-list', detail: '일반사용자 및 사업주사용자 회원가입'},
+      {title: '1:1 문의 하기', component: QuestionListPage, flag: false, ios: 'ios-chatbubbles', md: 'md-chatbubbles', detail: '업체 별 1:1 문의 내역을 확인할 수 있습니다.'},
+      {title: '컨설팅 내역', component: ConsultingListPage, flag: false, ios: 'ios-list', md: 'md-list', detail: '컨설팅 내역을 확인할 수 있습니다.'},
+      {title: '계정 정보 보기', component: UserInfoDetailPage, flag: false, ios: 'ios-person', md: 'md-person', detail: '계정 상세 보기 및 수정이 가능합니다.'}
     ];
 
     events.subscribe('mypage:logined', () => {
