8105c243109c5539f04aece42c5d5bf63cdd9bee
ushawn <ushawn@jbnu.ac.kr>
2017-02-08 16:26:24 +0900

    탭 5개로 수정,사이드 메뉴 구성 수정

--- cozyhouzz-app/src/pages/tabs/tabs.ts
+++ cozyhouzz-app/src/pages/tabs/tabs.ts
@@ -4,6 +4,7 @@ import { RoomListPage } from '../roomInfo/room-list/room-list';
 // import {LatelyRoomListPage} from '../roomInfo/lately-room-list/lately-room-list';
 import {DibRoomListPage} from '../roomInfo/dib-room-list/dib-room-list';
 import { MyPage } from '../mypage/mypage';
+import {QuestionListPage} from '../mypage/question/question-list/question-list';
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -15,7 +16,7 @@ export class TabsPage {
   tab1Root: any = HomePage;
   tab2Root: any = RoomListPage;
   tab3Root: any = DibRoomListPage;
-  // tab4Root: any = DibRoomListPage;
+  tab4Root: any = QuestionListPage;
   tab5Root: any = MyPage;
   constructor() {
 
